56a2868bd36ed3226018b5d520492ff2ffc4e9a2
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-05-04 22:16:33 +0900

    セキュリティグループなど設定

--- main.tf
+++ main.tf
@@ -1,9 +1,15 @@
 resource "aws_instance" "this" {
   ami           = "ami-0df2ca8a354185e1e"
   instance_type = "t2.micro"
-  subnet_id = "subnet-0ea856de19a8c03c8"
+  subnet_id     = "subnet-0ea856de19a8c03c8"
+
+  vpc_security_group_ids = [aws_security_group.this.id]
 
   tags = {
     Name = "terraform-sample"
   }
+}
+
+resource "aws_security_group" "this" {
+  name = "terraform-sample-SG"
 }
