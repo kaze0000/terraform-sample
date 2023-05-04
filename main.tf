6fbae9b2f0481342f9debec167ff40506c8a0330
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-05-04 22:17:00 +0900

    fix

--- main.tf
+++ main.tf
@@ -5,6 +5,8 @@ resource "aws_instance" "this" {
 
   vpc_security_group_ids = [aws_security_group.this.id]
 
+  associate_public_ip_address = true
+
   tags = {
     Name = "terraform-sample"
   }
@@ -12,4 +14,33 @@ resource "aws_instance" "this" {
 
 resource "aws_security_group" "this" {
   name = "terraform-sample-SG"
+}
+
+# インバウンドルール、アウトバウンドルールの設定
+resource "aws_security_group_rule" "ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+resource "aws_security_group_rule" "http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
+}
+
+# nginxをインストールする予定なので以下を追加して、外に出る通信を許可する
+resource "aws_security_group_rule" "egress" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.this.id
 }
