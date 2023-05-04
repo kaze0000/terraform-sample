1ee25ba0cb5f34d9ddbf1c7f764144aa9a45452a
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-05-04 22:56:43 +0900

    ユーザデータでnginxをインストール

--- main.tf
+++ main.tf
@@ -7,6 +7,16 @@ resource "aws_instance" "this" {
 
   associate_public_ip_address = true
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo amazon-linux-extras install -y nginx1
+              sudo yum install -y nginx
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+  user_data_replace_on_change = true
+
   tags = {
     Name = "terraform-sample"
   }
