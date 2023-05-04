8c09cdbb223deed1d48ff1fdbb051c8a534e8d13
kaze0000 <kazuya.00000.ds25@gmail.com>
2023-05-04 20:33:39 +0900

    terraform init

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "this" {
   ami           = "ami-0df2ca8a354185e1e"
   instance_type = "t2.micro"
+  subnet_id = "subnet-0ea856de19a8c03c8"
 
   tags = {
     Name = "terraform-sample"
